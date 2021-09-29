21357558a1b0ccb2531a120aeddbc60952debcda
Md-Imam-Hasan <imamhasan0007@gmail.com>
2021-09-29 19:13:56 +0600

    first commit

--- output/script.js
+++ output/script.js
@@ -9,10 +9,28 @@ class Player {
         console.log(`${this.name} from ${this.country} is playing`);
     }
 }
+// class shorthand when using access modifiers
+class Player1 {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing`);
+    }
+}
 const sakib = new Player("Sakib", 34, "Bangladesh");
 const mashrafi = new Player("Mashrafi", 37, "Bangladesh");
-sakib.play();
-mashrafi.play();
+// name is public, so it can be modified and accessed outside of class
+sakib.name = "Sakib Al Hasan";
+console.log(sakib.name);
+// age is private, so it can not be modified or accessed outside of class
+sakib.age = 33;
+console.log(sakib.age);
+// country is readonly, so it can not be modified but accessed outside of class
+sakib.country = "Baharain";
+console.log(sakib.country);
 const players = [];
 players.push(sakib);
 players.push(mashrafi);
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,7 @@
 class Player {
-  name: string;
-  age: number;
-  country: string;
+  public name: string;
+  private age: number;
+  readonly country: string;
 
   constructor(name: string, age: number, country: string) {
     this.name = name;
@@ -14,11 +14,33 @@ class Player {
   }
 }
 
+// class shorthand when using access modifiers
+class Player1 {
+  constructor(
+    public name: string,
+    private age: number,
+    readonly country: string
+  ) {}
+
+  play() {
+    console.log(`${this.name} from ${this.country} is playing`);
+  }
+}
+
 const sakib = new Player("Sakib", 34, "Bangladesh");
 const mashrafi = new Player("Mashrafi", 37, "Bangladesh");
 
-sakib.play();
-mashrafi.play();
+// name is public, so it can be modified and accessed outside of class
+sakib.name = "Sakib Al Hasan";
+console.log(sakib.name);
+
+// age is private, so it can not be modified or accessed outside of class
+sakib.age = 33;
+console.log(sakib.age);
+
+// country is readonly, so it can not be modified but accessed outside of class
+sakib.country = "Baharain";
+console.log(sakib.country);
 
 const players: Player[] = [];
 
